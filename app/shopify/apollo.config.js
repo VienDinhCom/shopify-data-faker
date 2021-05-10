c805674e04d0dadb45a84d11d7935896dd1da75e
Vien Dinh <maxvien@gmail.com>
2021-05-10 13:01:03 +0700

    micro changes

--- app/shopify/apollo.config.js
+++ app/shopify/apollo.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const envKit = require('env-kit');
 
-const schema = path.join(__dirname, 'schema.gql');
+const endpoint = envKit.get('SHOPIFY_GRAPHQL_ENDPOINT');
+const accessToken = envKit.get('SHOPIFY_ACCESS_TOKEN');
+
 const generated = path.join(__dirname, 'generated.ts');
 const documents = path.join(__dirname, 'graphql/**/*.gql');
 
@@ -8,7 +11,10 @@ module.exports = {
   client: {
     service: {
       name: 'shopify',
-      localSchemaFile: schema,
+      url: endpoint,
+      headers: {
+        'X-Shopify-Access-Token': accessToken,
+      },
     },
     excludes: [generated],
     includes: [documents],
