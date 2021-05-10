212e3dbc2b838640b5fb92352ee6c57a6f97f161
Vien Dinh <maxvien@gmail.com>
2021-05-10 11:06:23 +0700

    update codegen.config

--- app/shopify/codegen.config.js
+++ app/shopify/codegen.config.js
@@ -1,12 +1,16 @@
+const path = require('path');
 const envKit = require('env-kit');
 
 const endpoint = envKit.get('SHOPIFY_GRAPHQL_ENDPOINT');
 const accessToken = envKit.get('SHOPIFY_ACCESS_TOKEN');
 
+const generated = path.join(__dirname, 'generated.ts');
+const documents = path.join(__dirname, 'graphql/**/*.gql');
+
 module.exports = {
   overwrite: true,
   generates: {
-    'app/shopify/generated.ts': {
+    [generated]: {
       schema: [
         {
           [endpoint]: {
@@ -16,7 +20,7 @@ module.exports = {
           },
         },
       ],
-      documents: 'app/shopify/graphql/**/*.gql',
+      documents,
       plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
     },
   },
